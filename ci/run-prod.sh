44aa21ed6bb2b3720bc251797272f0f8e421eba6
franck <franck.rst@gmail.com>
2020-09-07 13:41:38 +0200

    WIP

--- ci/run-prod.sh
+++ ci/run-prod.sh
@@ -2,4 +2,5 @@
 
 envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < \
   /usr/local/srs/conf/srs.conf.template > /usr/local/srs/conf/srs.conf && \
+  mkdir -p /opt/data/hls && chmod 755 /opt/data/hls && \
   ./objs/srs -c conf/srs.conf
