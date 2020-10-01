bba2aaf0362d1c424928fdb91479adde59af41e2
franck <franck.rst@gmail.com>
2020-10-01 13:38:04 +0200

    fix docker

--- ci/run-prod.sh
+++ ci/run-prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < \
-  /usr/local/srs/conf/srs.conf.template > /usr/local/srs/conf/srs.conf && \
+  /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf && \
   mkdir -p /opt/data/hls && chmod 755 /opt/data/hls && \
-  ./objs/srs -c conf/srs.conf
+  npm run start & nginx -g "daemon off;"
+
