df9bfeee92205bbef4ef16c4300eff7095e425de
franck <franck.rst@gmail.com>
2020-10-01 14:16:49 +0200

    fix docker

--- ci/run-prod.sh
+++ ci/run-prod.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+mv /usr/app/config/prod.json /usr/app/config/prod.json.template
+
 envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < \
-  /usr/app/config/prod.json > /usr/app/config/prod.json && \
+  /usr/app/config/prod.json.template > /usr/app/config/prod.json && \
   mkdir -p /opt/data/hls && chmod 755 /opt/data/hls && \
   npm run start & nginx -g "daemon off;"
 
